012f4bfffa401f4207c9a8bf2efafb2a712594be
muzishuiyi <2270128887@qq.com>
2021-04-28 13:12:42 +0800

    fix bug

--- django_backend/BUAA/scheduled.py
+++ django_backend/BUAA/scheduled.py
@@ -5,7 +5,7 @@ import backend.settings as settings
 
 def get_access_token():
     response = requests.get(
-        f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={settings.APPID}&secret={settings.APPSECRET}')
+        f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={settings.APPID}&secret={settings.SECRET}')
     response = response.json()
     if response.get('access_token', ''):
         cache.set('access_token', response['access_token'])
