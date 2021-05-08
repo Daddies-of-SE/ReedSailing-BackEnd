4d5762cd34c4e06c9b21c3bf28a1c7871c038867
muzishuiyi <2270128887@qq.com>
2021-05-08 22:00:16 +0800

    Update settings.py

--- django_backend/backend/settings.py
+++ django_backend/backend/settings.py
@@ -129,7 +129,7 @@ CACHES = {
     },
     "code": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://114.116.94.235:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
