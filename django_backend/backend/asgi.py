a4c2a52daf58605eef61a978e96d59384c0a6c49
乐洋 <yueyang130@qq.com>
2021-05-17 13:56:59 +0800

    final version for wss

--- django_backend/backend/asgi.py
+++ django_backend/backend/asgi.py
@@ -19,7 +19,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabulator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django.setup()
 
